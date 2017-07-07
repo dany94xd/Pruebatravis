802ee83dfb82ffe85793de8e496dd6cdb345e399
dany <danimaxi60@hotmail.com>
2017-07-07 05:51:40 -0500

    prueba1

--- test.php
+++ test.php
@@ -5,15 +5,12 @@ class StackTest extends TestCase
 {
     public function testPushAndPop()
     {
-        $stack = [];
-        $this->assertEquals(0, count($stack));
+        
+        $this->assertEquals(1+1,2);
+        
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+      
+        $this->assertEquals('Home'($title));
     }
 }
 ?>
